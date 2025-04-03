9bf41921904216a432b024a1bf32d361f4d64fc5
Timur Badretdinov <accounts@destiner.io>
2025-04-03 19:13:15 +0100

    fix: rename validatorSet → ownerSet

--- src/services/modules.ts
+++ src/services/modules.ts
@@ -41,27 +41,27 @@ const WEBAUTHN_VALIDATOR_ADDRESS: Address =
   '0x2f167e55d42584f65e2e30a748f41ee75a311414'
 
 function toOwners(config: RhinestoneAccountConfig) {
-  const validatorSet = config.owners
-  switch (validatorSet.type) {
+  const ownerSet = config.owners
+  switch (ownerSet.type) {
     case 'ecdsa':
-      return validatorSet.accounts.map((account) => account.address)
+      return ownerSet.accounts.map((account) => account.address)
     case 'passkey':
       return [zeroAddress]
   }
 }
 
 function getValidator(config: RhinestoneAccountConfig) {
-  const validatorSet = config.owners
-  switch (validatorSet.type) {
+  const ownerSet = config.owners
+  switch (ownerSet.type) {
     case 'ecdsa':
       return getOwnableValidator({
-        threshold: validatorSet.threshold ?? validatorSet.accounts.length,
-        owners: validatorSet.accounts.map((account) => account.address),
+        threshold: ownerSet.threshold ?? ownerSet.accounts.length,
+        owners: ownerSet.accounts.map((account) => account.address),
       })
     case 'passkey':
       return getWebAuthnValidator({
-        pubKey: validatorSet.account.publicKey,
-        authenticatorId: validatorSet.account.id,
+        pubKey: ownerSet.account.publicKey,
+        authenticatorId: ownerSet.account.id,
       })
   }
 }
