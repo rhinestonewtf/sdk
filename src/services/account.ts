7957150882962c27c922fc2ced5624f9f2785cd7
Timur Badretdinov <accounts@destiner.io>
2025-04-07 09:14:59 +0100

    fix: store initcode as hashed

--- src/services/account.ts
+++ src/services/account.ts
@@ -25,11 +25,11 @@ import { RhinestoneAccountConfig } from '../types'
 import { getValidator, toOwners, RHINESTONE_ATTESTER_ADDRESS } from './modules'
 
 async function getAddress(chain: Chain, config: RhinestoneAccountConfig) {
-  const { factory, salt, initcode } = await getDeployArgs(chain, config)
+  const { factory, salt, hashedInitcode } = await getDeployArgs(chain, config)
   const hash = keccak256(
     encodePacked(
       ['bytes1', 'address', 'bytes32', 'bytes'],
-      ['0xff', factory, salt, keccak256(initcode)],
+      ['0xff', factory, salt, hashedInitcode],
     ),
   )
   const address = slice(hash, 12, 32)
@@ -185,14 +185,14 @@ async function getDeployArgs(
         ),
       )
 
-      const initcode: Hex =
-        '0x608060405234801561001057600080fd5b506040516101e63803806101e68339818101604052602081101561003357600080fd5b8101908080519060200190929190505050600073ffffffffffffffffffffffffffffffffffffffff168173ffffffffffffffffffffffffffffffffffffffff1614156100ca576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260228152602001806101c46022913960400191505060405180910390fd5b806000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505060ab806101196000396000f3fe608060405273ffffffffffffffffffffffffffffffffffffffff600054167fa619486e0000000000000000000000000000000000000000000000000000000060003514156050578060005260206000f35b3660008037600080366000845af43d6000803e60008114156070573d6000fd5b3d6000f3fea264697066735822122003d1488ee65e08fa41e58e888a9865554c535f2c77126a82cb4c0f917f31441364736f6c63430007060033496e76616c69642073696e676c65746f6e20616464726573732070726f766964656400000000000000000000000029fcb43b46531bca003ddc8fcb67ffe91900c762'
+      const hashedInitcode: Hex =
+        '0xe298282cefe913ab5d282047161268a8222e4bd4ed106300c547894bbefd31ee'
 
       return {
         factory: proxyFactory,
         factoryData,
         salt,
-        initcode,
+        hashedInitcode,
       }
     }
   }
