c06a4f5babde4be050b5378bed6ef358d43be546
Timur Badretdinov <accounts@destiner.io>
2025-04-29 10:33:49 +0100

    docs: JSDoc

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,36 @@ import {
 } from './execution'
 import type { RhinestoneAccountConfig, Session, Transaction } from './types'
 
+/**
+ * Initialize a Rhinestone account
+ * Note: accounts are deployed onchain only when the first transaction is sent.
+ * @param config Account config (e.g. implementation vendor, owner signers, smart sessions)
+ * @returns account
+ */
 async function createRhinestoneAccount(config: RhinestoneAccountConfig) {
+  /**
+   * Sign and send a transaction
+   * @param transaction Transaction to send
+   * @returns transaction result object (a bundle ID or a UserOp hash)
+   */
   function sendTransactions(transaction: Transaction) {
     return sendTransactionInternal(config, transaction)
   }
 
+  /**
+   * Wait for the transaction execution onchain
+   * @param result transaction result object
+   * @returns bundle result or a UserOp receipt
+   */
   function waitForExecution(result: TransactionResult) {
     return waitForExecutionInternal(config, result)
   }
 
-  function getAddress() {
+  /**
+   * Get account address
+   * @returns Address of the smart account
+   */
+  function getAddress(): string {
     return getAddressInternal(config)
   }
 
