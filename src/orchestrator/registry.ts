e9e70034b293755e59ed1ca64e1675dabcc7aa9b
Timur Badretdinov <accounts@destiner.io>
2025-05-02 14:18:11 +0100

    fix: typing

--- src/orchestrator/registry.ts
+++ src/orchestrator/registry.ts
@@ -242,15 +242,15 @@ function getTokenBalanceSlot(
   )
 }
 
-function getHookAddress() {
+function getHookAddress(): Address {
   return '0x0000000000f6Ed8Be424d673c63eeFF8b9267420'
 }
 
-function getSameChainModuleAddress() {
+function getSameChainModuleAddress(): Address {
   return '0x000000000043ff16d5776c7F0f65Ec485C17Ca04'
 }
 
-function getTargetModuleAddress() {
+function getTargetModuleAddress(): Address {
   return '0x0000000000E5a37279A001301A837a91b5de1D5E'
 }
 
