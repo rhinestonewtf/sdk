413225d12e8557f75027f3add7b386c1782679fe
Timur Badretdinov <accounts@destiner.io>
2025-05-19 13:53:24 +0100

    fix: type test consts

--- test/consts.ts
+++ test/consts.ts
@@ -1,16 +1,16 @@
-import { toWebAuthnAccount } from 'viem/account-abstraction'
-import { privateKeyToAccount } from 'viem/accounts'
+import { toWebAuthnAccount, WebAuthnAccount } from 'viem/account-abstraction'
+import { Account, privateKeyToAccount } from 'viem/accounts'
 
-const accountA = privateKeyToAccount(
+const accountA: Account = privateKeyToAccount(
   '0x2be89d993f98bbaab8b83f1a2830cb9414e19662967c7ba2a0f43d2a9125bd6d',
 )
-const accountB = privateKeyToAccount(
+const accountB: Account = privateKeyToAccount(
   '0x39e2fec1a04c088f939d81de8f1abebdebf899a6cfb9968f9b663a7afba8301b',
 )
-const accountC = privateKeyToAccount(
+const accountC: Account = privateKeyToAccount(
   '0xb63c74af219a3949cf95f5e3a3d20b0137425de053bb03e5cc0f46fe0d19f22f',
 )
-const passkeyAccount = toWebAuthnAccount({
+const passkeyAccount: WebAuthnAccount = toWebAuthnAccount({
   credential: {
     id: '9IwX9n6cn-l9SzqFzfQXvDHRuTM',
     publicKey:
