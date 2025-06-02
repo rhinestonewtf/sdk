7192d54de0ee5555e28616089a4675478872baae
Timur Badretdinov <accounts@destiner.io>
2025-06-02 16:58:04 +0100

    fix: run tests sequentially

---
+++ test/e2e.test.ts
@@ -0,0 +1,33 @@
+import { setupOrchestratorMock } from './orchestrator'
+import { setupViemMock } from './utils/viem'
+
+const deployerPrivateKey =
+  '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+const deployerAccount = privateKeyToAccount(deployerPrivateKey)
+
+const sourceChain = base
+const anvil = getAnvil(sourceChain, getForkUrl(sourceChain))
+
+setupOrchestratorMock()
+setupViemMock(anvil, deployerAccount)
+
+import { privateKeyToAccount } from 'viem/accounts'
+import { base } from 'viem/chains'
+import { beforeAll, describe } from 'vitest'
+
+import './utils/polyfill'
+import { getAnvil } from './utils/anvil'
+import { getForkUrl } from './utils/utils'
+
+// Import the test cases without their own setup
+import { runDeploymentTestCases } from './deployment'
+import { runBundlesTestCases } from './bundles'
+
+describe.sequential('E2E Tests', () => {
+  beforeAll(async () => {
+    await anvil.start()
+  })
+
+  runDeploymentTestCases()
+  runBundlesTestCases()
+})
