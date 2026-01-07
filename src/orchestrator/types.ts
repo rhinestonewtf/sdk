04ba4f41db7717b26352b95824123d481348d3ed
Diego Braga <dsbrgg@gmail.com>
2026-01-07 15:50:26 +0000

    exact input intents

--- src/orchestrator/types.ts
+++ src/orchestrator/types.ts
@@ -33,6 +33,7 @@ type IntentStatus =
 type AccountAccessListLegacy = {
   chainId: number
   tokenAddress: Address
+  amount?: bigint
 }[]
 
 type MappedChainTokenAccessList = {
@@ -384,6 +385,7 @@ export type {
   Portfolio,
   PortfolioToken,
   Execution,
+  AccountAccessList,
   MappedChainTokenAccessList,
   UnmappedChainTokenAccessList,
   TokenRequirements,
